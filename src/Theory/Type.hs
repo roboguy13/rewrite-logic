9ecd54676190901b6590674d55ffe9eb8c1ae4aa
roboguy13 <roboguy06@gmail.com>
2021-03-25 00:07:33 -0500

    More towards new rewrite rule definition style

--- src/Theory/Type.hs
+++ src/Theory/Type.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE DeriveFoldable #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 {-# OPTIONS_GHC -Wall -Wno-unused-imports #-}
 
@@ -43,6 +44,20 @@ data RuleVar =
   ProdVar String | RuleVar String
   deriving (Show)
 
+newtype UnifierVar = UnifierVar String deriving (Show)
+
+instance Ppr UnifierVar where
+  ppr (UnifierVar str) = '?' : str
+
+data WffRewrite =
+  WffRewrite
+    { wffRewriteLhs :: Formula (String, UnifierVar)
+    , wffRewriteRhs :: Formula UnifierVar
+    }
+
+-- wffRewriteToRewrite :: [Production'] -> WffRewrite -> Either String (Rewrite Wff')
+-- wffRewriteToRewrite = undefined
+
 data WffVar a = WffVarName String | WffVarFilled a deriving (Show, Data, Functor, Traversable, Foldable)
 
 instance Applicative WffVar where
@@ -60,7 +75,7 @@ data Wff'
   | WffEmpty
   | WffSpace
   -- | WffWff Wff
-  | WffRuleVar String Wff'
+  -- | WffRuleVar String Wff'
   deriving (Show, Data)
 
 -- Apply once, left-to-right
@@ -91,84 +106,38 @@ flattenWff' (WffJuxt (WffJuxt xs:ys)) =
       WffJuxt xs' ->
         case flattenWff' (WffJuxt (map flattenWff' ys)) of
           WffJuxt ys' -> WffJuxt (xs' ++ ys')
+          z -> z
+      z -> z
 flattenWff' (WffJuxt (x:xs)) =
   case flattenWff' (WffJuxt (map flattenWff' xs)) of
     WffJuxt xs' -> WffJuxt (flattenWff' x : xs')
+    z -> z
 flattenWff' x = x
 
-
-instance Unify Wff' where
-  type UnifyEnv Wff' = [(String, Wff')]
-
-  unifyWith env0 x0 y0 = strength1 $ runState (fmap (fmap flattenWff') (go x0 y0)) env0
-    where
-      go :: Wff' -> Wff' -> State (UnifyEnv Wff') (Maybe Wff')
-      go w@(WffTerminal x) (WffTerminal y)
-        | x == y = return $ Just w
-      go WffEmpty WffEmpty = return $ Just WffEmpty
-      go WffSpace WffSpace = return $ Just WffSpace
-      go w@(WffJuxt []) (WffJuxt []) = return $ Just w
-      go (WffJuxt (x:xs)) (WffJuxt (y:ys)) = do
-        z <- go x y
-        zs <- sequence $ zipWith go xs ys
-        let r = (:) <$> z <*> sequence zs
-        return $ fmap WffJuxt r
-      go w@(WffRuleVar x p) (WffRuleVar y q)
-        | x == y && isJust (unify p q) = do
-            env <- get
-            case lookup x env of
-              Just z -> return $ Just z
-              Nothing -> return $ Just w
-        | otherwise = return Nothing  -- Basic unification here
-      go (WffRuleVar x p) y = do
-        env <- get
-        case lookup x env of
-          Nothing ->
-            case unify p y of
-            put ((x,y):env)
-            return $ Just y
-          Just z -> go y z
-      go _ _ = return Nothing
-
-  substUnifyEnv env = flattenWff' . transform go
-    where
-      go w@(WffRuleVar x) =
-        case lookup x env of
-          Just y -> y
-          Nothing -> w
-      go w = w
 
 instance Postprocess Wff' where
   postprocess = flattenWff'
 
--- instance Unify Wff' where
---   unifyWith env (SimpleWff x) (SimpleWff y) = fmap go (unifyWith env x y)
---     where
---       go = undefined
---   unifyWith env (WffJuxt x xs) (WffJuxt y ys) = do
---     (z, env') <- unifyWith env x y
---     (zs, env'') <- unify env' xs ys
---     return (WffJuxt z zs, env'')
 
-wff'EqToRewrite :: Theory' a -> Equality Wff' -> Rewrite Wff'
-wff'EqToRewrite th eql@(x :=: y) = rewriteWithErr (ppr eql) $ \z ->
-  case unify x z of
-    Just (_, env) -> Just (substUnifyEnv env y)
-    Nothing -> Nothing
-    -- then Just y
-    -- else Nothing
+-- wff'EqToRewrite :: Theory' a -> Equality Wff' -> Rewrite Wff'
+-- wff'EqToRewrite th eql@(x :=: y) = rewriteWithErr (ppr eql) $ \z ->
+--   case unify x z of
+--     Just (_, env) -> Just (substUnifyEnv env y)
+--     Nothing -> Nothing
+--     -- then Just y
+--     -- else Nothing
 
--- -- wff'RewriteWff :: Rewrite Wff' -> Rewrite Wff
--- -- wff'RewriteWff re = rewrite $ \w ->
--- --   case runRewrite re (wffParsed w) of
--- --     Nothing -> Nothing
--- --     Just x -> Just $ w { wffParsed = x }
+-- -- -- wff'RewriteWff :: Rewrite Wff' -> Rewrite Wff
+-- -- -- wff'RewriteWff re = rewrite $ \w ->
+-- -- --   case runRewrite re (wffParsed w) of
+-- -- --     Nothing -> Nothing
+-- -- --     Just x -> Just $ w { wffParsed = x }
 
--- -- wffEqToRewrite :: Theory' a -> Equality Wff -> Rewrite Wff
--- -- wffEqToRewrite th (x :=: y) = wff'RewriteWff (wff'EqToRewrite th (wffParsed x :=: wffParsed y))
+-- -- -- wffEqToRewrite :: Theory' a -> Equality Wff -> Rewrite Wff
+-- -- -- wffEqToRewrite th (x :=: y) = wff'RewriteWff (wff'EqToRewrite th (wffParsed x :=: wffParsed y))
 
-wffEqToRewrite' :: Theory' a -> Equality Wff -> Rewrite Wff'
-wffEqToRewrite' th (x :=: y) = wff'EqToRewrite th (wffParsed x :=: wffParsed y)
+-- wffEqToRewrite' :: Theory' a -> Equality Wff -> Rewrite Wff'
+-- wffEqToRewrite' th (x :=: y) = wff'EqToRewrite th (wffParsed x :=: wffParsed y)
 
 data Wff =
   Wff
@@ -177,21 +146,21 @@ data Wff =
   }
   deriving (Show, Data)
 
--- instance Ppr SimpleWff where
---   ppr (WffTerminal t) = t
---   ppr WffEmpty = "<empty>"
---   ppr WffSpace = "<space>"
---   ppr (WffRuleVar v) = '?' : v
+-- -- instance Ppr SimpleWff where
+-- --   ppr (WffTerminal t) = t
+-- --   ppr WffEmpty = "<empty>"
+-- --   ppr WffSpace = "<space>"
+-- --   ppr (WffRuleVar v) = '?' : v
 
 instance Ppr Wff' where
   ppr (WffJuxt ws) = unwords (map ppr ws)
   ppr (WffTerminal t) = t
   ppr WffEmpty = "<empty>"
   ppr WffSpace = "<space>"
-  ppr (WffRuleVar v) = '?' : v
+  -- ppr (WffRuleVar v) = '?' : v
 
-instance Ppr Wff where
-  ppr (Wff name w) = '<' : name ++ "> ::= " ++ ppr w
+-- instance Ppr Wff where
+--   ppr (Wff name w) = '<' : name ++ "> ::= " ++ ppr w
 
 data NumProd = NumProd String String String
   deriving (Show)
@@ -206,8 +175,8 @@ data Theory' a
     deriving Show
 type Theory = Theory' Wff
 
-theoryRewrites :: Theory -> [(String, Rewrite Wff')]
-theoryRewrites th = map (second (wffEqToRewrite' th)) $ theoryRules th
+-- theoryRewrites :: Theory -> [(String, Rewrite Wff')]
+-- theoryRewrites th = map (second (wffEqToRewrite' th)) $ theoryRules th
 
 instance Parseable RuleVar where
   parse = fmap RuleVar ruleVar <|> fmap ProdVar parseMetaVar'
